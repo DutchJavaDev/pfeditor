8025ef49b4f3258623797546157748af5df6cbcb
Boris <Borismulder@outlook.com>
2022-05-06 14:23:54 +0200

    create page for creating new items
    based of a project*

--- lib/create.dart
+++ lib/create.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'data/blueprint.dart';
+import 'api/backend.dart' as api;
 
 class CreatePage extends StatefulWidget {
   const CreatePage({Key? key}) : super(key: key);
@@ -9,42 +10,88 @@ class CreatePage extends StatefulWidget {
 }
 
 class _CreatePage extends State<CreatePage> {
-  final _template = ProjectBlueprint("TTitle", "Des", "Url");
+  final _template = ProjectBlueprint("Title", "Des", "Url");
+  final _inputFields = ["Title", "Description"];
+  late List<TextEditingController> _controllers;
+  String _dropDownValue = 'Select a project';
+
+  @override
+  void initState() {
+    super.initState();
+    _controllers = [
+      TextEditingController(),
+      TextEditingController(),
+    ];
+
+    for (var i = 0; i < _controllers.length; i++) {
+      var controller = _controllers[i];
+      controller.addListener(() {
+        final text = controller.text;
+        switch (i) {
+          case 0:
+            _template.title = text;
+            break;
+
+          case 1:
+            _template.description = text;
+            break;
+
+          default:
+            break;
+        }
+      });
+    }
+  }
 
   List<Widget> createForm() {
     var children = List<Widget>.empty(growable: true);
 
-    // Title, Description, GitHub URL fields
-    children.add(const Padding(
-      padding: EdgeInsets.only(top: 15, bottom: 5),
-      child: TextField(
-        decoration: InputDecoration(
-          border: OutlineInputBorder(),
-          labelText: 'Title',
+    for (var i = 0; i < _inputFields.length; i++) {
+      children.add(Padding(
+        padding: const EdgeInsets.only(top: 15, bottom: 5),
+        child: TextField(
+          controller: _controllers[i],
+          maxLines: i == 1 ? 12 : 1,
+          decoration: InputDecoration(
+              border: const OutlineInputBorder(),
+              labelText: _inputFields[i],
+              alignLabelWithHint: true),
         ),
-      ),
-    ));
+      ));
+    }
 
-    children.add(const Padding(
-      padding: EdgeInsets.only(top: 15, bottom: 5),
-      child: TextField(
-        maxLines: 12,
-        decoration: InputDecoration(
-          border: OutlineInputBorder(),
-          alignLabelWithHint: true,
-          labelText: 'Description',
-        ),
-      ),
-    ));
-
-    children.add(const Padding(
-      padding: EdgeInsets.only(top: 15, bottom: 5),
-      child: TextField(
-        decoration: InputDecoration(
-          border: OutlineInputBorder(),
-          labelText: 'Github URL',
-        ),
-      ),
+    children.add(Padding(
+      padding: const EdgeInsets.only(top: 15, bottom: 5),
+      child: FutureBuilder<List<String>>(
+          future: api.fecthProjects(),
+          builder:
+              (BuildContext context, AsyncSnapshot<List<String>> snapshot) {
+            if (snapshot.hasData) {
+              snapshot.data?.insert(0, "Select a project");
+              return DropdownButton<String>(
+                  value: _dropDownValue,
+                  icon: const Icon(Icons.arrow_downward),
+                  items: snapshot.data
+                      ?.map<DropdownMenuItem<String>>((String str) {
+                    return DropdownMenuItem<String>(
+                      value: str,
+                      child: Text(str),
+                      enabled:
+                          str.startsWith("Select a project") ? false : true,
+                    );
+                  }).toList(),
+                  onChanged: (String? s) {
+                    setState(() {
+                      _dropDownValue = s!;
+                      _template.url = _dropDownValue.split(" ")[1];
+                    });
+                  });
+            } else if (snapshot.hasError) {
+              return const Center(child: Text("Error"));
+            } else {
+              return const Center(child: Text("Loading"));
+            }
+          }),
     ));
 
     children.add(Padding(
@@ -55,7 +102,9 @@ class _CreatePage extends State<CreatePage> {
           child: TextButton(
             child: const Text("Save",
                 style: TextStyle(fontSize: 18, color: Colors.white)),
-            onPressed: () {},
+            onPressed: () {
+              print(_template.toJson());
+            },
             style: ButtonStyle(
               backgroundColor:
                   MaterialStateProperty.all<Color>(Colors.blueGrey),
@@ -82,5 +131,13 @@ class _CreatePage extends State<CreatePage> {
             children: createForm(),
           ),
         )));
+  }
+
+  @override
+  void dispose() {
+    for (var c in _controllers) {
+      c.dispose();
+    }
+    super.dispose();
   }
 }
