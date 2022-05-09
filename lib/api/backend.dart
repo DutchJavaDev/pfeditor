4452c3a9670dd9534a21c86dfc35ec6707901859
Boris <Borismulder@outlook.com>
2022-05-09 02:17:41 +0200

    fixed saving and loading json data + login simulation
    did a double jsonEncode, caused a parsing error when saving new entry

--- lib/api/backend.dart
+++ lib/api/backend.dart
@@ -1,5 +1,12 @@
 import 'dart:convert';
+import 'dart:io';
 import 'package:http/http.dart' as http;
+import 'package:path_provider/path_provider.dart' as path;
+
+import '../data/blueprint.dart';
+
+var _pathPrefix = "pfeditorData";
+var _fileName = "pfeditor_json_data.json";
 
 Future<List<String>> fecthProjects() async {
   var projects = List<String>.empty(growable: true);
@@ -13,4 +20,58 @@ Future<List<String>> fecthProjects() async {
   }
 
   return projects;
+}
+
+Future<Directory> getTemporaryDirectory() async {
+  var documentsDir = await path.getApplicationDocumentsDirectory();
+  var tempDir =
+      Directory("${documentsDir.path}${Platform.pathSeparator}$_pathPrefix");
+  if (!await tempDir.exists()) {
+    return await tempDir.create(recursive: true);
+  } else {
+    return tempDir;
+  }
+}
+
+void saveProject(Map<String, dynamic> projectblueprint) async {
+  var tempDir = await getTemporaryDirectory();
+
+  var file = File("${tempDir.path}${Platform.pathSeparator}$_fileName");
+
+  if (await file.exists()) {
+    var newEntry = projectblueprint;
+
+    var list = jsonDecode(file.readAsStringSync()) as List<dynamic>;
+
+    list.add(newEntry);
+
+    file.writeAsStringSync(jsonEncode(list));
+  } else {
+    var newEntry = [projectblueprint];
+    file = await file.create();
+    file.writeAsStringSync(jsonEncode(newEntry));
+  }
+}
+
+Future<String> loadProject() async {
+  var tempDir = await getTemporaryDirectory();
+  var file = File("${tempDir.path}/$_fileName");
+
+  if (!await file.exists()) {
+    file = await file.create();
+    var list = [
+      ProjectBlueprint("Sim", "Ba", "").toJson(),
+      ProjectBlueprint("King", "Kong", "").toJson(),
+      ProjectBlueprint("AG", "S", "").toJson(),
+    ];
+
+    file.writeAsStringSync(jsonEncode(list));
+  }
+
+  return file.readAsStringSync();
+}
+
+Future<bool> simulateLogin(String email, String password) async {
+  await Future.delayed(const Duration(seconds: 1));
+  return true;
 }
