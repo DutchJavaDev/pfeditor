9e47971573d41f66b5d1efcd23b93da664ac9a7f
Boris <Borismulder@outlook.com>
2022-05-09 02:18:46 +0200

    using routeobserver to update future builder on navigator pop or push

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,12 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:pfeditor/create.dart';
+import 'api/backend.dart' as api;
+import 'data/blueprint.dart';
+
+final RouteObserver<ModalRoute<void>> observer =
+    RouteObserver<ModalRoute<void>>();
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -8,7 +15,37 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePage();
 }
 
-class _HomePage extends State<HomePage> {
+class _HomePage extends State<HomePage> with RouteAware {
+  bool hasBeenBuild = false;
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    observer.subscribe(this, ModalRoute.of(context)!);
+  }
+
+  @override
+  void dispose() {
+    observer.unsubscribe(this);
+    super.dispose();
+  }
+
+  @override
+  void didPush() {
+    // Route was pushed onto navigator and is now topmost route.
+    _updateListView();
+  }
+
+  @override
+  void didPopNext() {
+    // Covering route was popped off the navigator.
+    _updateListView();
+  }
+
+  void _updateListView() async {
+    if (!hasBeenBuild) return;
+    setState(() {});
+  }
+
   List<Widget> createChildren(double containerWidth) {
     var children = List<Widget>.empty(growable: true);
     double padding = 1;
@@ -21,31 +58,28 @@ class _HomePage extends State<HomePage> {
             child: Padding(
               padding: EdgeInsets.all(padding),
               child: Container(
-                  width: containerWidth / 2 - padding,
-                  height: containerWidth / 4 - padding,
-                  decoration: BoxDecoration(
+                width: containerWidth / 2 - padding,
+                height: containerWidth / 4 - padding,
+                decoration: BoxDecoration(
                     color: Colors.green,
-                    borderRadius: BorderRadius.circular(5)
-                  ),
-                  child: Center(child: Text("$i")),
-                ),
+                    borderRadius: BorderRadius.circular(5)),
+                child: Center(child: Text("$i")),
+              ),
             ),
           ),
           SizedBox(
-           width: containerWidth / 2,
+            width: containerWidth / 2,
             height: containerWidth / 4,
             child: Padding(
-              padding: EdgeInsets.all(padding),
-              child: Container(
+                padding: EdgeInsets.all(padding),
+                child: Container(
                   width: containerWidth / 2 - padding,
                   height: containerWidth / 4 - padding,
                   decoration: BoxDecoration(
-                    color: Colors.green,
-                    borderRadius: BorderRadius.circular(5)
-                  ),
-                  child: Text("${i*2}"),
-                )
-            ),
+                      color: Colors.green,
+                      borderRadius: BorderRadius.circular(5)),
+                  child: Text("${i * 2}"),
+                )),
           )
         ],
       ));
@@ -58,7 +92,7 @@ class _HomePage extends State<HomePage> {
   Widget build(BuildContext context) {
     var appWidth = MediaQuery.of(context).size.width;
     var containerWidth = appWidth;
-
+    hasBeenBuild = true;
     return Scaffold(
       appBar: AppBar(
         title: const Text("Bonjour"),
@@ -68,16 +102,34 @@ class _HomePage extends State<HomePage> {
             tag: "landing",
             child: SizedBox(
               width: containerWidth,
-              child: ListView(
-                children: createChildren(containerWidth),
-                
-              ),
+              child: FutureBuilder(
+                  future: api.loadProject(),
+                  builder:
+                      (BuildContext context, AsyncSnapshot<String> snapshot) {
+                    if (snapshot.hasData) {
+                      // Own widget
+                      var projects = jsonDecode(snapshot.data!);
+
+                      return ListView.builder(
+                        itemCount: projects.length,
+                        itemBuilder: (context, index) {
+                          var project =
+                              ProjectBlueprint.fromJson(projects[index]);
+                          return Center(child: Text(project.title));
+                        },
+                      );
+                    } else if (snapshot.hasError) {
+                      return Text("Error: ${snapshot.error}");
+                    } else {
+                      return const Center(child: Text("Loading beep boop"));
+                    }
+                  }),
             )),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          Navigator.push(context, MaterialPageRoute(builder: (context){
-              return const CreatePage();
+          Navigator.push(context, MaterialPageRoute(builder: (context) {
+            return const CreatePage();
           }));
         },
         child: const Icon(Icons.add),
