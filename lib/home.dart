b1c4e6c8c97a660ef1b2bd396ba223fa70fc5c8f
Boris <Borismulder@outlook.com>
2022-05-12 17:36:28 +0200

    Update home.dart
    final card layout for this build

--- lib/home.dart
+++ lib/home.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:pfeditor/create.dart';
 import 'api/backend.dart' as api;
 import 'data/blueprint.dart';
+import 'dart:math' as math;
 
 final RouteObserver<ModalRoute<void>> observer =
     RouteObserver<ModalRoute<void>>();
@@ -74,27 +75,68 @@ class _HomePage extends State<HomePage> with RouteAware {
                             const SliverGridDelegateWithFixedCrossAxisCount(
                           crossAxisCount: 4,
                         ),
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 10, horizontal: 20),
                         itemCount: api.projectsCount,
                         itemBuilder: (context, index) {
                           var project =
                               ProjectBlueprint.fromJson(projects[index]);
-                          return Column(
-                            children: [
-                              Flexible(child: Text(project.title), flex: 1),
-                              Flexible(
-                                  child: Text(project.description), flex: 5),
-                              Flexible(child: Text(project.url), flex: 3),
-                              Flexible(
-                                child: TextButton(
-                                    onPressed: () {
-                                      activeProjectId = index;
+                          return Card(
+                            color: Colors.grey,
+                            child: Padding(
+                              padding: const EdgeInsets.only(
+                                  left: 5.0, right: 5.0, top: 5.0),
+                              child: Column(
+                                children: [
+                                  Flexible(
+                                    child: Container(
+                                      child: Center(child: Text(project.title)),
+                                      color: Color((math.Random().nextDouble() *
+                                                  0xFFFFFF)
+                                              .toInt())
+                                          .withOpacity(1.0),
+                                    ),
+                                    flex: 1,
+                                  ),
+                                  Flexible(
+                                    child: Container(
+                                      width: double.infinity,
+                                      height: double.infinity,
+                                      child: Padding(
+                                        padding: const EdgeInsets.all(5.0),
+                                        child: Text(project.description),
+                                      ),
+                                      color: Color((math.Random().nextDouble() *
+                                                  0xFFFFFF)
+                                              .toInt())
+                                          .withOpacity(1.0),
+                                    ),
+                                    flex: 3,
+                                  ),
+                                  Flexible(
+                                    child: Container(
+                                      child: Center(child: Text(project.url)),
+                                      color: Color((math.Random().nextDouble() *
+                                                  0xFFFFFF)
+                                              .toInt())
+                                          .withOpacity(1.0),
+                                    ),
+                                    flex: 1,
+                                  ),
+                                  Flexible(
+                                    child: TextButton(
+                                        onPressed: () {
+                                          activeProjectId = index;
 
-                                      Navigator.pushNamed(context, '/create');
-                                    },
-                                    child: const Text("Edit")),
-                                flex: 4,
-                              )
-                            ],
+                                          Navigator.pushNamed(
+                                              context, '/create');
+                                        },
+                                        child: const Text("Edit")),
+                                    flex: 1,
+                                  )
+                                ],
+                              ),
+                            ),
                           );
                         },
                       );
